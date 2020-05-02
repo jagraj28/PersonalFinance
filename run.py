569a5680f0671d12dae02d202585d74676ef8cbd
Jagraj Singh <singhjagraj900@gmail.com>
2020-05-03 00:20:24 +0100

    v2.3
    Successfully implemented sqlalchemy using new class adding_assets. Next step is using database results to output to My Account page.

--- run.py
+++ run.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from forms import add_assets, add_investments, add_debts, LoginForm, RegistrationForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class adding_assets(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    bank = db.Column(db.String(20), unique=True, nullable=False)
+    amount = db.Column(db.Integer, unique=True, nullable=False)
+    interest = db.Column(db.Integer, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"adding_assets('{self.bank}', '{self.amount}', '{self.interest}')"
 
 @app.route('/')
 @app.route('/home')
@@ -29,10 +42,17 @@ def login():
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
+@app.route('/account', methods=['GET', 'POST'])
+def account():
+    return render_template('account.html')
+
 @app.route('/assets', methods=['GET', 'POST'])
 def assets():
     form = add_assets()
     if form.validate_on_submit():
+        asset = adding_assets(bank=form.bank.data, amount=form.amount.data, interest=form.interest.data)
+        db.session.add(asset)
+        db.session.commit()
         flash(f'Changes made saved!', 'success')
         return redirect(url_for('home'))
     else:
