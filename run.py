8c9293fb2cd70b904c2ad64551a6703f4e6387f1
Jagraj Singh <singhjagraj900@gmail.com>
2020-05-02 02:11:11 +0100

    v3
    Lots of work done, created html pages for all 3 sub pages as well as routing links.

--- run.py
+++ run.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/assets/')
+def assets():
+    return render_template('assets.html')
+
+@app.route('/investments/')
+def investments():
+    return render_template('investments.html')
+
+@app.route('/debts/')
+def debts():
+    return render_template('debts.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
